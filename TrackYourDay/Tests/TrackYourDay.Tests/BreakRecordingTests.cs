9f38354727e440d7dd31a35a66ecb45230e01adb
Adam <ss.skuty@gmail.com>
2023-06-15 23:17:56 +0200

    Add high level tests for basic features

---
+++ TrackYourDay/Tests/TrackYourDay.Tests/BreakRecordingTests.cs
@@ -0,0 +1,26 @@
+using System.Threading.Channels;
+using System;
+
+namespace TrackYourDay.Tests
+{
+    public class BreakRecordingTests
+    {
+        [Fact]
+        public void GivenBreakRecordingFeatureIsEnabled_WhenThereIsNoActivityForSpecifiedInFeatureSettingsAmountOfTime_ThenBreakIsRecorded()
+        {
+            Assert.Fail("Feature not implemented");
+        }
+
+        [Fact]
+        public void GivenBreakRecordingFeatureIsEnabled_WhenUserSessionInOperatingSystemIsBlocked_ThenBreakIsRecorded()
+        {
+            Assert.Fail("Feature not implemented");
+        }
+
+        [Fact]
+        public void GivenBreakRecordingFeatureIsEnabled_WhenBreakRecordingEnds_ThenUserCanChooseToRegisterRecordedBreak()
+        {
+            Assert.Fail("Feature not implemented");
+        }
+    }
+}
---
+++ TrackYourDay/Tests/TrackYourDay.Tests/EventRegisteringTests.cs
@@ -0,0 +1,16 @@
+using System.Threading.Channels;
+using System;
+
+namespace TrackYourDay.Tests
+{
+    public class EventRegisteringTests
+    {
+        [Fact]
+        public void WhenActiveWindowChanges_ThenEventIsRegistered()
+        {
+            ///--Event Date  
+            ///--Active Window name
+            Assert.Fail("Feature not implemented");
+        }
+    }
+}
