9e76d2e91017b57e435fdbe2dbc8905768fd060d
Adam <ss.skuty@gmail.com>
2023-06-23 18:07:54 +0200

    Add basic tests for checking version of application

---
+++ TrackYourDay/Tests/TrackYourDay.Tests/VersionCheckingTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using TrackYourDay.Core.Tasks;
+
+namespace TrackYourDay.Tests
+{
+    public class VersionCheckingTests
+    {
+        [Fact]
+        public void ReturnsVersionOfCurrentApplication()
+        {
+            Assert.Fail("Not implemented");
+        }
+
+        [Fact]
+        public void ReturnsVersionOfNewestAvailableApplication()
+        {
+            Assert.Fail("Not implemented");
+        }
+
+        [Fact]
+        public void ReturnsTrueIfNewerVersionIsAvailable()
+        {
+            Assert.Fail("Not implemented");
+        }
+    }
+}
