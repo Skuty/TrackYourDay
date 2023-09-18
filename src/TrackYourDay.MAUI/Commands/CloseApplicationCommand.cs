50994a996ad780d59e1fa87b2bd4dfbdfe64e35d
Adam <ss.skuty@gmail.com>
2023-09-18 22:52:25 +0200

    Add services and copy Summary page

---
+++ src/TrackYourDay.MAUI/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace TrackYourDay.MAUI.Commands
+{
+    internal class CloseApplicationCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            Application.Current.Quit();
+        }
+    }
+}
--- src/TrackYourDay.MAUI/MauiProgram.cs
+++ src/TrackYourDay.MAUI/MauiProgram.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using TrackYourDay.Core.Activities;
+using TrackYourDay.Core.Breaks;
+using TrackYourDay.Core.Old.Activities.RecognizingStrategies;
+using TrackYourDay.Core;
 using TrackYourDay.MAUI.Data;
+using Quartz;
+using TrackYourDay.MAUI.BackgroundJobs;
 
 namespace TrackYourDay.MAUI
 {
@@ -14,15 +21,29 @@ namespace TrackYourDay.MAUI
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
-
             builder.Services.AddMauiBlazorWebView();
 
 #if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Services.AddBlazorWebViewDeveloperTools();
 		builder.Logging.AddDebug();
 #endif
 
             builder.Services.AddSingleton<WeatherForecastService>();
+            builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<ActivityTracker>());
+            builder.Services.AddSingleton<IClock, Clock>();
+            builder.Services.AddScoped<IStartedActivityRecognizingStrategy, DefaultActivityRecognizingStategy>();
+            builder.Services.AddScoped<IInstantActivityRecognizingStrategy, DefaultInstantActivityRecognizingStrategy>();
+            builder.Services.AddSingleton<ActivityTracker>();
+            builder.Services.AddSingleton<BreakTracker>(serviceCollection => new BreakTracker(
+                serviceCollection.GetRequiredService<IPublisher>(),
+                serviceCollection.GetRequiredService<IClock>(),
+                TimeSpan.FromMinutes(5)));
+            // Install notification handler
+            builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<ActivityStartedNotificationHandler>());
+
+            builder.Services.AddQuartz(o => o.UseMicrosoftDependencyInjectionJobFactory());
+            builder.Services.AddQuartzHostedService();
+
 
             return builder.Build();
         }
