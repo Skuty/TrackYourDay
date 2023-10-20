d8800394e847ded6cd61c966a55887ed9a3b79f9
Adam <ss.skuty@gmail.com>
2023-10-20 22:02:51 +0200

    Add more tests for worktime, overtime and breaks

--- Tests/TrackYourDay.Tests/Workday/BreaksLeftTests.cs
+++ Tests/TrackYourDay.Tests/Workday/BreaksLeftTests.cs
@@ -2,5 +2,22 @@
 {
     public class BreaksLeftTests
     {
+        [Fact]
+        public void GivenThereWasNoBreaks_WhenBreaksAreBeingCalculated_ThenBreaksLeftAreEqualTo50Minutes()
+        {
+        }
+
+        [Fact]
+        public void GivenThereWas15MinutesOfBreaks_WhenBreaksAreBeingCalculated_ThenBreaksLeftAreEqualTo35Minutes()
+        {
+        }
+
+        [Fact]
+        public void GivenThereWas50MinutesOfBreaks_WhenBreaksAreBeingCalculated_ThenBreaksLeftAreEqualTo0Minutes()
+        {
+        }
+
+        // TODO: split in summary activities and breaks to avoid problems with properly calculating break time
+        // due to lack of possibility to fully autorecognize breaks - temporarily
     }
 }
--- Tests/TrackYourDay.Tests/Workday/OverhoursTests.cs
+++ Tests/TrackYourDay.Tests/Workday/OverhoursTests.cs
@@ -2,5 +2,24 @@
 {
     public class OverhoursTests
     {
+        [Fact]
+        public void GivenThereWasNoActivitiesOrBreaks_WhenOverhoursAreBeingCalculated_ThenOverhoursEqualsTo0Hours()
+        {
+        }
+
+        [Fact]
+        public void GivenThereWas7HoursAnd10MinutesOfActivitiesAnd50MinutesOfBreaks_WhenOverhoursAreBeingCalculated_ThenOverhoursEqualsTo0Hours()
+        {
+        }
+
+        [Fact]
+        public void GivenThereWas8HoursOfActivitiesAnd50MinutesOfBreaks_WhenOverhoursAreBeingCalculated_ThenOverhoursEqualsTo50Minutes()
+        {
+        }
+
+        [Fact]
+        public void GivenThereWas9HoursAnd40MinutesOfActivitiesAnd50MinutesOfBreaks_WhenOverhoursAreBeingCalculated_ThenOverhoursEqualsTo2HoursAnd30Minutes()
+        {
+        }
     }
 }
