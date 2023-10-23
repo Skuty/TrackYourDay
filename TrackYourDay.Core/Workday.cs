8b03ad6f99b41fb3195d62750c44d86072431865
Adam <ss.skuty@gmail.com>
2023-10-23 22:14:54 +0200

    Correct display and left comment for future

--- TrackYourDay.Core/Workday.cs
+++ TrackYourDay.Core/Workday.cs
@@ -1,4 +1,5 @@
-﻿using TrackYourDay.Core.Activities;
+﻿using System.Collections.ObjectModel;
+using TrackYourDay.Core.Activities;
 using TrackYourDay.Core.Breaks;
 
 namespace TrackYourDay.Core
@@ -6,23 +7,25 @@ namespace TrackYourDay.Core
     //TODO: Reconisder and write down what is the purpose of this class
     // What is WorkDay, worktimeleft, allWorkTime, etc
     // Existing assumptions are conflicting with each other
+
+    //TODO: Add tests for not tested parts or extract them
     public class Workday
     {
         public TimeSpan TimeOfAllActivities { get; }
 
         public TimeSpan TimeOfAllBreaks { get; }
 
-        public TimeSpan WorktimeLeft { get; }
-        
-        //TODO: Add tests for this
+        public TimeSpan BreaksLeft { get; }
+
         public TimeSpan ValidBreaksUsed { get; }
 
-        public TimeSpan BreaksLeft { get; }
+        public TimeSpan WorktimeLeft { get; }
         
         public TimeSpan Overhours { get; }
 
-        public static Workday CreateBasedOn(List<EndedActivity> endedActivities, List<EndedBreak> endedBreaks)
+        public static Workday CreateBasedOn(IReadOnlyCollection<EndedActivity> endedActivities, IReadOnlyCollection<EndedBreak> endedBreaks)
         {
+            //TODO: Verify because it is substituing Breaks
             var timeOfAllActivities = endedActivities.Aggregate(TimeSpan.Zero, (durationSum, activity) => durationSum + activity.GetDuration());
             
             var timeOfAllBreaks = endedBreaks.Aggregate(TimeSpan.Zero, (durationSum, @break) => durationSum + @break.BreakDuration);
