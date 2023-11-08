6eb774ae06e3e26c22cd20cdb4cb8137970ccfd0
Adam <ss.skuty@gmail.com>
2023-11-08 19:44:48 +0100

    Workday refactoring

--- TrackYourDay.Core/Workday.cs
+++ TrackYourDay.Core/Workday.cs
@@ -32,19 +32,19 @@ namespace TrackYourDay.Core
         public TimeSpan OverallTimeLeftToWork { get; }
 
         /// <summary>
-        /// Amount of Time which Employee already worked
-        /// </summary>
-        // TODO: Add missing unit tests
-        public TimeSpan TimeAlreadyActivelyWorkded { get; }
-
-        /// <summary>
         /// Amount of Time which Employee should work to fullfill regulation requirements.
         /// This time does not include Breaks
         /// </summary>
         public TimeSpan TimeLeftToWorkActively { get; }
 
         /// <summary>
-        /// Amount of Time which Employee worked more than regulation requirements
+        /// Amount of Time which Employee already worked
+        /// This time does not include Breaks
+        /// </summary>
+        public TimeSpan TimeAlreadyActivelyWorkded { get; }
+
+        /// <summary>
+        /// Amount of Time which Employee Actively worked more than regulation requirements
         /// </summary>
         public TimeSpan OverhoursTime { get; }
 
@@ -58,13 +58,62 @@ namespace TrackYourDay.Core
         /// </summary>
         public TimeSpan ValidBreakTimeUsed { get; }
 
+        private Workday(TimeSpan timeLeftToWork, TimeSpan breaksLeft, TimeSpan overhours, TimeSpan timeOfAllActivities, TimeSpan timeOfAllBreaks, TimeSpan validBreaksUsed)
+        {
+            TimeLeftToWorkActively = timeLeftToWork;
+            BreakTimeLeft = breaksLeft;
+            OverhoursTime = overhours;
+            TimeOfAllActivities = timeOfAllActivities;
+            TimeOfAllBreaks = timeOfAllBreaks;
+            ValidBreakTimeUsed = validBreaksUsed;
+        }
+
         public static Workday CreateBasedOn(IReadOnlyCollection<EndedActivity> endedActivities, IReadOnlyCollection<EndedBreak> endedBreaks)
         {
-            //TODO: Verify because it is substituing Breaks
-            var timeOfAllActivities = endedActivities.Aggregate(TimeSpan.Zero, (durationSum, activity) => durationSum + activity.GetDuration());
-            
-            var timeOfAllBreaks = endedBreaks.Aggregate(TimeSpan.Zero, (durationSum, @break) => durationSum + @break.BreakDuration);
+            var timeOfAllActivities = GetTimeOfAllActivities(endedActivities);
+            var timeOfAllBreaks = GetTimeOfAllBreaks(endedBreaks);
+            var breakTimeLeft = GetTimeOfBreaksLeft(timeOfAllBreaks);
+            var validBreaksUsed = GetTimeOfValidBreaksUsed(timeOfAllBreaks);
+            var timeLeftToWork = GetTimeLeftToWork(timeOfAllActivities, timeOfAllBreaks);
+            var overhours = GetOverhours(timeLeftToWork);
 
+            return new Workday(
+                timeLeftToWork,
+                breakTimeLeft,
+                overhours,
+                timeOfAllActivities,
+                timeOfAllBreaks,
+                validBreaksUsed);
+        }
+
+        private static TimeSpan GetOverhours(TimeSpan timeLeftToWork)
+        {
+            return timeLeftToWork < TimeSpan.Zero ? timeLeftToWork * -1 : TimeSpan.Zero;
+        }
+
+        private static TimeSpan GetTimeLeftToWork(TimeSpan timeOfAllActivities, TimeSpan timeOfAllBreaks)
+        {
+            var timeLeftToWork = Config.WorkdayDuration - Config.AllowedBreakDuration - (timeOfAllActivities - timeOfAllBreaks);
+            return timeLeftToWork >= TimeSpan.Zero ? timeLeftToWork : TimeSpan.Zero;
+        }
+
+        private static TimeSpan GetTimeOfValidBreaksUsed(TimeSpan timeOfAllBreaks)
+        {
+            TimeSpan validBreaksUsed;
+            if (timeOfAllBreaks.TotalSeconds > Config.AllowedBreakDuration.TotalSeconds)
+            {
+                validBreaksUsed = Config.AllowedBreakDuration;
+            }
+            else
+            {
+                validBreaksUsed = timeOfAllBreaks;
+            }
+
+            return validBreaksUsed;
+        }
+
+        private static TimeSpan GetTimeOfBreaksLeft(TimeSpan timeOfAllBreaks)
+        {
             TimeSpan breaksLeft;
             //TODO: Extract static config from here 
             if (timeOfAllBreaks.TotalSeconds > Config.AllowedBreakDuration.TotalSeconds)
@@ -76,37 +125,18 @@ namespace TrackYourDay.Core
                 breaksLeft = Config.AllowedBreakDuration - timeOfAllBreaks;
             }
 
-            TimeSpan validBreaksUsed;
-            if (timeOfAllBreaks.TotalSeconds > Config.AllowedBreakDuration.TotalSeconds)
-            {
-                validBreaksUsed = Config.AllowedBreakDuration;
-            }
-            else
-            {
-                validBreaksUsed = timeOfAllBreaks;
-            }
-
-            var timeLeftToWork = Config.WorkdayDuration - Config.AllowedBreakDuration - (timeOfAllActivities - timeOfAllBreaks);
-
-            var overhours = timeLeftToWork < TimeSpan.Zero ? timeLeftToWork * -1 : TimeSpan.Zero;
-
-            return new Workday(
-                timeLeftToWork >= TimeSpan.Zero ? timeLeftToWork : TimeSpan.Zero,
-                breaksLeft,
-                overhours, 
-                timeOfAllActivities, 
-                timeOfAllBreaks,
-                validBreaksUsed);
+            return breaksLeft;
         }
 
-        private Workday(TimeSpan timeLeftToWork, TimeSpan breaksLeft, TimeSpan overhours, TimeSpan timeOfAllActivities, TimeSpan timeOfAllBreaks, TimeSpan validBreaksUsed)
+        private static TimeSpan GetTimeOfAllBreaks(IReadOnlyCollection<EndedBreak> endedBreaks)
         {
-            TimeLeftToWorkActively = timeLeftToWork;
-            BreakTimeLeft = breaksLeft;
-            OverhoursTime = overhours;
-            TimeOfAllActivities = timeOfAllActivities;
-            TimeOfAllBreaks = timeOfAllBreaks;
-            ValidBreakTimeUsed = validBreaksUsed;
+            return endedBreaks.Aggregate(TimeSpan.Zero, (durationSum, @break) => durationSum + @break.BreakDuration);
         }
+
+        private static TimeSpan GetTimeOfAllActivities(IReadOnlyCollection<EndedActivity> endedActivities)
+        {
+            return endedActivities.Aggregate(TimeSpan.Zero, (durationSum, activity) => durationSum + activity.GetDuration());
+        }
+
     }
 }
