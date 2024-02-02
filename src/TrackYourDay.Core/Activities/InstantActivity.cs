f781b10873718fbd3e5a29ba524284dd1f028fb0
Adam <ss.skuty@gmail.com>
2024-02-03 00:51:14 +0100

    Subscribe to Instant Activities and add them for Break Processing, extend Instant Activity with Guid

--- src/TrackYourDay.Core/Activities/InstantActivity.cs
+++ src/TrackYourDay.Core/Activities/InstantActivity.cs
@@ -1,7 +1,7 @@
 ﻿using TrackYourDay.Core.Activities.SystemStates;
 namespace TrackYourDay.Core.Activities
 {
-    public record class InstantActivity(DateTime OccuranceDate, SystemState SystemState)
+    public record class InstantActivity(Guid Guid, DateTime OccuranceDate, SystemState SystemState)
     {
         public TimeSpan GetDuration()
         {
