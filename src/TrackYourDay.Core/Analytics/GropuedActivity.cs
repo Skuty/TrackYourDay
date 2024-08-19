282a0ccb49132e4f7db30397db39565262eaf9f8
Adam <ss.skuty@gmail.com>
2024-08-19 23:11:34 +0200

    Add draft of Grouped Activity

---
+++ src/TrackYourDay.Core/Analytics/GropuedActivity.cs
@@ -0,0 +1,24 @@
+﻿using TrackYourDay.Core.Activities;
+
+namespace TrackYourDay.Core.Analytics
+{
+    internal class GropuedActivity
+    {
+        private List<Guid> processedActivities { get; set; }
+
+        public DateOnly Date { get; }
+
+        public string ActivityDescription { get; }
+
+        public TimeSpan Duration { get; private set; }
+
+        public void Include(EndedActivity activity) 
+        {
+            if (!this.processedActivities.Contains(null))
+            {
+                this.Duration += activity.GetDuration();
+                this.processedActivities.Add(null);
+            }
+        }
+    }
+}
