4f7825cf310df73858f2e59d9c1766aca67def1f
Adam <ss.skuty@gmail.com>
2024-09-11 21:42:37 +0200

    Adjust Updating Script

--- src/TrackYourDay.MAUI/UpdateApplication.bat
+++ src/TrackYourDay.MAUI/UpdateApplication.bat
@@ -3,6 +3,9 @@ setlocal EnableDelayedExpansion
 
 echo Welcome to TrackYourDay Application Updater!
 echo Your Personal Data won't be deleted.
+
+echo ----------------------------------------------------
+
 echo Content of following directory will be deleted: %CD%
 
 pause
@@ -18,6 +21,8 @@ for /D %%D in (*) do (
 )
 echo All files and subdirectories deleted except Updater.
 
+echo ----------------------------------------------------
+
 echo Downloading newest Version from GitHub Releases.
 powershell -NoProfile -Command ^
     "$latestRelease = Invoke-RestMethod -Uri 'https://api.github.com/repos/Skuty/TrackYourDay/releases/latest' -Headers @{ 'User-Agent' = 'Mozilla/5.0' };" ^
@@ -29,11 +34,40 @@ echo Extracting TrackYourDay_NewestRelease.zip.
 powershell -Command "Expand-Archive -Path 'TrackYourDay_NewestRelease.zip' -DestinationPath . -Force"
 echo Extraction completed.
 
+echo ----------------------------------------------------
+
 echo Deleting TrackYourDay_NewestRelease.zip
 del TrackYourDay_NewestRelease.zip
 echo Zip archive deleted.
 
+echo ----------------------------------------------------
+
+echo TrackYourDay Updated.
+
+echo ----------------------------------------------------
+
+echo TrackYourDay will be added to Autostart.
+
+pause
+
+set startupFolder="%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+set trackYourDayShortcut=%startupFolder%\TrackYourDay.lnk
+
+powershell -Command ^
+    "$WScriptShell = New-Object -ComObject WScript.Shell;" ^
+    "$Shortcut = $WScriptShell.CreateShortcut('%trackYourDayShortcut%');" ^
+    "$Shortcut.TargetPath = '%CD%\TrackYourDay.exe';" ^
+    "$Shortcut.Save();"
+
+echo TrackYourDay has been added to Autostart.
+
+echo ----------------------------------------------------
+
 echo Process completed successfully!
-echo Please run TrackYourDay Application manually.
+
+echo ----------------------------------------------------
+
+echo Now TrackYourDay will be started.
+start "" "%CD%\TrackYourDay.MAUI.exe"
 
 pause
