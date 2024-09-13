a4ef158ae9864fff375eed22b825ca899dbbde84
Skuty <ss.skuty@gmail.com>
2024-09-13 19:45:02 +0200

    Feat: Add new Tips for The Day

--- src/TrackYourDay.MAUI/UiNotifications/TipForDayNotification.cs
+++ src/TrackYourDay.MAUI/UiNotifications/TipForDayNotification.cs
@@ -11,9 +11,20 @@ namespace TrackYourDay.MAUI.UiNotifications
 {
     public class TipForDayNotification : ExecutableNotification
     {
+        private readonly List<string> advices;
+
         public TipForDayNotification() : base()
         {
             this.IsEnabled = true;
+            this.advices = new List<string>();
+            this.advices.Add("Porada na dziś: Zacznij kończyć, przestań zaczynać :)");
+            this.advices.Add("Porada na dziś: Zaakceptuj niedoskonałe, nie wszystko musi być idealne :)");
+            this.advices.Add("Porada na dziś: Szukaj okazji, a nie przeszkód :)");
+            this.advices.Add("Porada na dziś: Uśmiechnij się :]");
+            this.advices.Add("Porada na dziś: Pamiętaj o przerwach! Twój Organizm ich potrzebuje!");
+            this.advices.Add("Czy wiesz że: Drzewa emitują związki chemiczne zwane fitoncydami, które mają właściwości antybakteryjne. Badania wykazały, że przebywanie w lasach i wdychanie tych związków może wspomagać układ odpornościowy człowieka.");
+            this.advices.Add("Czy wiesz że: Mikrobiom jelitowy, czyli zbiór mikroorganizmów żyjących w naszym układzie pokarmowym, wpływa na nasz nastrój, zdrowie psychiczne, trawienie, a nawet układ odpornościowy. Właściwa dieta wspierająca dobre bakterie ma zatem bezpośredni wpływ na nasze samopoczucie.");
+            this.advices.Add("Około 75% roślin uprawnych na świecie jest zależnych od zapylaczy, takich jak pszczoły. Dzięki nim mamy dostęp do wielu owoców i warzyw, bez których nasza dieta byłaby znacznie uboższa.");
         }
 
         public override bool ShouldBeExecuted()
@@ -25,9 +36,11 @@ namespace TrackYourDay.MAUI.UiNotifications
         {
             base.Execute();
 
+            var advice = this.advices[Random.Shared.Next(this.advices.Count)];
+
             MauiPageFactory.OpenSimpleNotificationPageInNewWindow(new SimpleNotificationViewModel(
                     "Miłego dnia pracy!",
-                    $"Porada na dziś: Zacznij kończyć, przestań zaczynać :)"));
+                    advice));
         }
     }
 }
