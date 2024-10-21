019f9180443a5a47a986d5fb115dee4171e153bd
Adam <ss.skuty@gmail.com>
2024-10-21 23:18:29 +0200

    Add description of next task to do

--- src/TrackYourDay.MAUI/PopupBlazorPage.xaml.cs
+++ src/TrackYourDay.MAUI/PopupBlazorPage.xaml.cs
@@ -7,5 +7,12 @@
             InitializeComponent();
             this.blazorWebViewSecond.StartPath = path;
         }
+
+        // Send MediatR Command to close / minimize this window directly from BlazorView
+        private void CloseThisWindow()
+        {
+            var currentWindow = Microsoft.Maui.Controls.Application.Current?.Windows.FirstOrDefault(w => w.Page is SimpleNotificationPage && w.Page.GetHashCode() == this.GetHashCode());
+            Microsoft.Maui.Controls.Application.Current?.CloseWindow(currentWindow);
+        }
     }
 }
