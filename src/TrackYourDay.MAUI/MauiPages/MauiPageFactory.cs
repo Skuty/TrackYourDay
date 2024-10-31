e11697a63c64b2b8e14a427cb23ee730443e290e
Adam <ss.skuty@gmail.com>
2024-10-31 23:02:40 +0100

    Restore old popup window height

--- src/TrackYourDay.MAUI/MauiPages/MauiPageFactory.cs
+++ src/TrackYourDay.MAUI/MauiPages/MauiPageFactory.cs
@@ -21,7 +21,7 @@ namespace TrackYourDay.MAUI.MauiPages
                 Window blazorPopUpPage = new Window(new PopupBlazorPage(path));
                 blazorPopUpPage.Title = $"Track Your Day - Pop Up";
                 blazorPopUpPage.Width = 385;
-                blazorPopUpPage.Height = 215;
+                blazorPopUpPage.Height = 115;
                 Application.Current.OpenWindow(blazorPopUpPage);
 
                 var localWindow = (blazorPopUpPage.Handler.PlatformView as Microsoft.UI.Xaml.Window);
