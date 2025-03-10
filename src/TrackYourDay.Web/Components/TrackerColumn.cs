b6c464327d376977f4807b8567968fe6548ba855
Adam <ss.skuty@gmail.com>
2025-03-10 22:28:05 +0100

    Draft of uniwersal tracking view component

---
+++ src/TrackYourDay.Web/Components/TrackerColumn.cs
@@ -0,0 +1,9 @@
+﻿namespace TrackYourDay.Web.Components
+{
+    public class TrackerColumn<T>
+    {
+        public string Title { get; set; }
+        public Expression<Func<T, object>> PropertyExpression { get; set; }
+        public bool Filterable { get; set; } = true;
+    }
+}
