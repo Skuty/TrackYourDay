7af841de2a9dc4c2416c665c4e049807c649b756
Adam <ss.skuty@gmail.com>
2025-03-27 20:52:55 +0100

    Add encryption service with text

---
+++ Tests/TrackYourDay.Tests/EncryptionServiceTests.cs
@@ -0,0 +1,70 @@
+﻿using FluentAssertions;
+using TrackYourDay.Core;
+
+namespace TrackYourDay.Tests
+{
+
+    public class EncryptionServiceTests
+    {
+        [Fact]
+        public void GivenSaltIsSupplied_WhenTextIsEncrypted_ThenResultsOfEncryptionIsDifferentThanOriginalText()
+        {
+            //Arrange
+            var salt = "salt";
+            var text = "text";
+            var encryptionService = new EncryptionService(salt);
+
+            // Act          
+            var encryptedText = encryptionService.Encrypt(text);
+
+            // Assert
+            encryptedText.Should().NotBe(text);
+        }
+
+        [Fact]
+        public void GivenSaltIsSupplied_WhenTextIsEncryptedAndDecrypted_ThenDecryptedTextIsEqualToOriginalText()
+        {
+            //Arrange
+            var salt = "salt";
+            var text = "text";
+            var encryptionService = new EncryptionService(salt);
+            
+            // Act
+            var encryptedText = encryptionService.Encrypt(text);
+            var decryptedText = encryptionService.Decrypt(encryptedText);
+
+            // Assert
+            decryptedText.Should().Be(text);
+        }
+
+        [Fact]
+        public void GivenSaltIsNotSupplied_WhenTextIsEncrypted_ThenResultsOfEncryptionIsDifferentThanOriginalText()
+        {
+            //Arrange
+            var text = "text";
+            var encryptionService = new EncryptionService(string.Empty);
+
+            // Act          
+            var encryptedText = encryptionService.Encrypt(text);
+
+            // Assert
+            encryptedText.Should().NotBe(text);
+        }
+
+        [Fact]
+        public void GivenSaltIsNotSupplied_WhenTextIsEncryptedAndDecrypted_ThenDecryptedTextIsEqualToOriginalText()
+        {
+            //Arrange
+            var salt = "salt";
+            var text = "text";
+            var encryptionService = new EncryptionService(salt);
+
+            // Act
+            var encryptedText = encryptionService.Encrypt(text);
+            var decryptedText = encryptionService.Decrypt(encryptedText);
+
+            // Assert
+            decryptedText.Should().Be(text);
+        }
+    }
+}
---
+++ src/TrackYourDay.Core/IEncryptionService.cs
@@ -0,0 +1,89 @@
+﻿using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
+
+namespace TrackYourDay.Core
+{
+    public interface IEncryptionService
+    {
+        string Encrypt(string text);
+    }
+
+    public class EncryptionService : IEncryptionService
+    {
+        private readonly string salt;
+
+        public EncryptionService(string salt)
+        {
+            if (!string.IsNullOrEmpty(salt))
+            {
+                this.salt = salt;
+            } 
+            else
+            {
+                this.salt = this.GetWindowsUserAccountId();
+            }
+        }
+
+        public string Encrypt(string text)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                var key = new Rfc2898DeriveBytes(salt, Encoding.UTF8.GetBytes(salt)).GetBytes(32);
+                aesAlg.Key = key;
+                aesAlg.GenerateIV();
+
+                var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (var msEncrypt = new MemoryStream())
+                {
+                    msEncrypt.Write(BitConverter.GetBytes(aesAlg.IV.Length), 0, sizeof(int));
+                    msEncrypt.Write(aesAlg.IV, 0, aesAlg.IV.Length);
+
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    using (var swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(text);
+                    }
+
+                    return Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+        }
+
+        public string Decrypt(string text)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                var key = new Rfc2898DeriveBytes(salt, Encoding.UTF8.GetBytes(salt)).GetBytes(32);
+                aesAlg.Key = key;
+                using (var msDecrypt = new MemoryStream(Convert.FromBase64String(text)))
+                {
+                    byte[] ivLengthBytes = new byte[sizeof(int)];
+                    msDecrypt.Read(ivLengthBytes, 0, ivLengthBytes.Length);
+                    var ivLength = BitConverter.ToInt32(ivLengthBytes, 0);
+                    var iv = new byte[ivLength];
+                    msDecrypt.Read(iv, 0, iv.Length);
+                    aesAlg.IV = iv;
+                    var decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                    using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    using (var srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        return srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+        }
+
+        private string? GetWindowsUserAccountId()
+        {
+            var sid = WindowsIdentity.GetCurrent().User;
+            if (sid == null)
+            {
+                return null;
+            }
+            return sid.Value;
+        }
+
+    }
+}
