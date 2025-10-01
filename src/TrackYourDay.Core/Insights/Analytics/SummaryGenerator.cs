351dd2dc9688831e1e139df2169e7e143a3eb82a
Adam <ss.skuty@gmail.com>
2025-10-01 21:00:18 +0200

    Add SummaryGenerator as clean Facade

---
+++ src/TrackYourDay.Core/Insights/Analytics/SummaryGenerator.cs
@@ -0,0 +1,10 @@
+﻿namespace TrackYourDay.Core.Insights.Analytics
+{
+    internal class SummaryGenerator
+    {
+        public IReadOnlyCollection<GroupedActivity> Generate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
